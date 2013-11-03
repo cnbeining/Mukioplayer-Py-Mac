6892f443016344ee1eb12ea3e968216c81c2cc04
superwbd <Beining@wifihost-83-141.wireless.utsc.utoronto.ca>
2013-11-03 12:21:34 -0500

    V2: Solve a security problem.

--- server.py
+++ server.py
@@ -6,13 +6,13 @@ import sys
 import webbrowser
 from multiprocessing import Process
 import threading
-
+import shutil
 
 
 def http_server():
-    user = getpass.getuser()
-    user_dir = '/Users/' + user
-    os.chdir(user_dir)
+    py_path = sys.path[0]
+    real_path = getrelpath(py_path)
+    os.chdir(py_path)
     from BaseHTTPServer import HTTPServer
     from SimpleHTTPServer import SimpleHTTPRequestHandler
     from SocketServer import ThreadingMixIn
@@ -21,7 +21,6 @@ def http_server():
     serveraddr=('', 8765)   # http://yige.org
     srvr=ThreadingServer(serveraddr,SimpleHTTPRequestHandler)
     srvr.serve_forever()
-    #pass
 
 
 
@@ -36,12 +35,24 @@ def getrelpath(input_file):
 
 
 def main(video_relpath, danmu_relpath):
-    danmu_relpath = getrelpath(danmu_relpath)
-    video_relpath = getrelpath(video_relpath)
+    #danmu_relpath = getrelpath(danmu_relpath)
+    #video_relpath = getrelpath(video_relpath)
+    video_filename = video_relpath.split("/")[-1]
+    danmu_filename = danmu_relpath.split("/")[-1]
     py_path = sys.path[0]
-    real_path = getrelpath(py_path)
     os.chdir(py_path)
-
+    user = getpass.getuser()
+    user_dir = '/Users/' + user
+    print(user_dir)
+    real_cache_dir = user_dir + '/.cache/mukioplayer_py'
+    os.system('rm -rf /'+real_cache_dir)
+    os.system('mkdir /'+real_cache_dir)
+    os.chdir(py_path)
+    os.system('ln -s '+ real_cache_dir+' ./mukiocache ')
+    os.system('cp /'+ video_relpath+'  '+ real_cache_dir)
+    os.system('cp /'+ danmu_relpath+'  '+ real_cache_dir)
+    print(danmu_filename)
+    print(danmu_filename)
     xml_to_write = '''<?xml version="1.0" encoding="utf-8"?>
 <conf>
   <performance>
@@ -52,20 +63,20 @@ def main(video_relpath, danmu_relpath):
   </performance>
   <server>
     <onhost>yes</onhost>
-    <load>http://localhost:8765/'''+danmu_relpath+'''</load>
+    <load>http://localhost:8765/mukiocache/mukioplayer_py/'''+ danmu_filename+'''</load>
     <send></send>
     <gateway></gateway>
   </server>
 </conf>
     '''
     xml_to_write = xml_to_write.encode("utf-8")
-
+    os.chdir(py_path)
     f = open('./conf.xml', "w")
 
     f.write(xml_to_write)
     f.close()
     Process(target=http_server, ).start()
-    webbrowser.open('http://localhost:8765/'+real_path+ '/mukioplayerplus.swf?file=http://localhost:8765/' + video_relpath+'&type=video&sort=new')
+    webbrowser.open('http://localhost:8765/mukioplayerplus.swf?file=http://localhost:8765/mukiocache/mukioplayer_py/' + video_filename+'&type=video&sort=new')
 
 
 v_relpath = str(input('Vid'))
